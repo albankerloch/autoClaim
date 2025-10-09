2faf4ef354fc8dabb91954ee9bfdf3f737aa774e
Alban Kerloc'h <alban.kerloch@gmail.com>
2025-10-09 15:45:20 +0000

    test talk ok

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,14 +1,20 @@
 import streamlit as st
-from elevenlabs import set_api_key, generate, save
 import os
 from dotenv import load_dotenv
+import tempfile
+from elevenlabs.client import ElevenLabs
+
 
 load_dotenv()
 EVENTLABS_API_KEY = os.environ.get("EVENTLABS_API_KEY")
 if not EVENTLABS_API_KEY:
     raise ValueError("Missing EVENTLABS_API_KEY")
 
-set_api_key(EVENTLABS_API_KEY)
+
+client = ElevenLabs(
+    api_key=EVENTLABS_API_KEY,
+    # éventuellement base_url si besoin selon region
+)
 
 st.title("🎙️ Text-to-Speech avec Evenlabs")
 
@@ -16,8 +22,16 @@ text = st.text_area("Entre ton texte :", "Bonjour, je suis une voix générée p
 
 if st.button("🎧 Générer l’audio"):
     if text.strip():
-        audio = generate(text, voice="fr-FR-Wavenet-D")
-        st.audio(audio, format="audio/wav")
+        audio = client.text_to_speech.convert(
+                text=text,
+                model_id="eleven_multilingual_v2",
+                voice_id="pmISDijbLDrzVXC8fEO0"  # Remplacez par une voix existante
+            )
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as f:
+            for chunk in audio:
+                f.write(chunk)
+            audio_path = f.name
+        st.audio(audio_path, format="audio/mp3")
         st.success("✅ Audio généré avec succès !")
     else:
         st.warning("❗ Entrez un texte avant de générer l’audio.")
